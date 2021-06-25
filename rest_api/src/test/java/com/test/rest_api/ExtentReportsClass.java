be36cb70681412cad10e22e6e348eb90e57a8b46
Krishna Kumar <krishnakwr579@gmail.com>
2021-06-25 20:56:47 +0530

    Extent

---
+++ rest_api/src/test/java/com/test/rest_api/ExtentReportsClass.java
@@ -0,0 +1,11 @@
+package com.test.rest_api;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
+public class ExtentReportsClass {
+	
+	ExtentReports extent;
+	ExtentTest logger;
+
+}
